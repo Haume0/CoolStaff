91242679055ef861fa7f4c175ed687e53dffcd23
Haume <70094846+Haume0@users.noreply.github.com>
2022-09-04 21:55:24 +0300

    Version -Diasma-
    *Main Screen and projects next tool released!
    *Router system added.
    *System optimized.

---
+++ src/components/anamenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bg-csmain w-screen h-screen">
+        <div class="flex justify-center place-items-center w-screen h-screen">
+            <div class="absolute w-[344px] h-[96px] flex items-center top-10">
+                <img src="../assets/coolstaff.svg" class="absolute" />
+                <span class="absolute font-Nunito font-extrabold text-4xl ml-28" >CoolStaff</span>
+                <span class="absolute font-Nunito font-semibold text-base ml-28 mt-14">Cool and open sourced tools.</span>
+            </div>
+            <div class="absolute bg-[#fff] h-[1px] w-[66%] top-44"></div>
+            <ul class="absolute flex w-[66%] h-[66%] justify-start mt-48">
+                <div id="foo" @click="goPG()" class="relative bg-csside rounded-2xl w-56 h-56 border-solid border-[#444444] border-[2px] cshover">
+                    <img src="../assets/coolpg.svg" class="absolute w-28 h-28 top-[calc(50%-90px)] left-[calc(50%-96px/2)]" />
+                    <div class="absolute w-24 h-8 bg-[#000]/70 csblur rounded-l-full right-0 top-4 flex justify-center place-items-center">
+                        <span class="font-Nunito font-bold text-based text-[#62EF89]">Released</span>
+                    </div>
+                    <div class="absolute flex justify-center w-full h-[30%] rounded-b-2xl bottom-0 bg-csmain">
+                            <span class="absolute w-[90%] text-center font-Nunito font-semibold text-base mt-2">Password Generator</span>
+                            <span class="absolute w-[100%] text-center font-Nunito font-normal text-[12px] mt-8">A powerfull password generator.</span>
+                    </div>
+                </div>
+                <div class="relative bg-csside rounded-2xl w-56 h-56 border-solid border-[#444444] border-[2px] ml-5 cshoverNO">
+                    <img src="../assets/coolgl.svg" class="absolute w-28 h-28 top-[calc(50%-90px)] left-[calc(50%-96px/2)]" />
+                    <div class="absolute w-24 h-8 bg-[#000]/70 csblur rounded-l-full right-0 top-4 flex justify-center place-items-center">
+                        <span class="font-Nunito font-bold text-sm text-[#EFB762]">Comin' Soon</span>
+                    </div>
+                    <div class="absolute flex justify-center w-full h-[30%] rounded-b-2xl bottom-0 bg-csmain">
+                        <span class="absolute w-[90%] text-center font-Nunito font-semibold text-base mt-2">Gradient Library</span>
+                        <span class="absolute w-[100%] text-center font-Nunito font-normal text-[12px] mt-8">Cool gradients for cool designs and arts.</span>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </div>
+</template>
+<script setup>
+import router from '../router';
+
+    function goPG(){
+        router.push({ path: '/pg'})
+    }
+</script>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import './index.css'
+import './style.css'
 import App from './App.vue'
 import './fonts/fonts.css'
+import router from './router/'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
