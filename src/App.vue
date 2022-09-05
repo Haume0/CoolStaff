53b166e2c095788d9b9b3e2da860b1bf27f4e9a7
Haume <70094846+Haume0@users.noreply.github.com>
2022-09-05 15:13:48 +0300

    Version -Triant-
    -Deign maded adaptive.
    -Optimized for phones.
    -

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-    <router-view />
+  <router-view />
 </template>
-<script setup>
+<script setup>   
+import { ref } from "vue";
+import $ from "jquery";
+$(document).ready(function () {
+  let screenS = window.screen.width;
+  if (screenS <= 1080) {
+    $(":root").css("--scale", "0.85");
+    console.log(screenS);
+  } else {
+    $(":root").css("--scale", "1");
+    console.log(screenS);
+  }
+});
+
+$(window).resize(function () {
+  let screenS = window.screen.width;
+  if (screenS <= 1080) {
+    $(":root").css("--scale", "0.85");
+    console.log(screenS);
+  } else {
+    $(":root").css("--scale", "1");
+    console.log(screenS);
+  }
+});
 </script>
 <style></style>
